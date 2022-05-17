55ff83c7f74921b5a4afb1719bc5a67512a52f70
DucPr0 <37231815+DucPr0@users.noreply.github.com>
2022-05-17 01:29:18 -0400

    minor sql script modification

--- TableCreationScript.sql
+++ TableCreationScript.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS Inventory
 CREATE TABLE Inventory (
-	id uniqueidentifier PRIMARY KEY DEFAULT newsequentialid(),
-	name varchar(100),
+  id uniqueidentifier PRIMARY KEY DEFAULT newsequentialid(),
+  name varchar(100),
   country varchar(100)
 )
